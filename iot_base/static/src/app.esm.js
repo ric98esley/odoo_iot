3399917b618d69a412e5b211a2be41ccbe7c28a4
Ricardo Perez <ricardo.pereze.98@gmail.com>
2025-10-06 14:54:54 -0400

    [FETCH] (app) init app

--- iot_base/static/src/app.esm.js
+++ iot_base/static/src/app.esm.js
@@ -1,5 +1,5 @@
-import {whenReady} from "@odoo/owl";
 import {mountComponent} from "@web/env";
 import {Root} from "./root.esm";
+import {whenReady} from "@odoo/owl";
 
 whenReady(() => mountComponent(Root, document.body));
